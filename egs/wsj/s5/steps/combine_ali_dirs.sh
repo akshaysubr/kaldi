akshaysubr/kaldi

commit d90d3545f8054efc3fb094660e581fb7ef74e8b8

fix the bug about seq and dash

egs/wsj/s5/steps/combine_ali_dirs.sh
@@ -68,9 +68,9 @@ echo "$0: dumping alignments in each source directory as single archive and inde
 for dir in $*; do
   src_id=$((src_id + 1))
   cur_num_jobs=$(cat $dir/num_jobs) || exit 1;
-  all_ids=$(seq -s, $cur_num_jobs)
+  alis=$(for n in $(seq $cur_num_jobs); do echo $dir/ali.$n.gz; done)
   $cmd $dir/log/copy_alignments.log \
-    copy-int-vector "ark:gunzip -c $dir/ali.{$all_ids}.gz|" \
+    copy-int-vector "ark:gunzip -c $alis|" \
     ark,scp:$temp_dir/ali.$src_id.ark,$temp_dir/ali.$src_id.scp || exit 1;
 done
 sort -m $temp_dir/ali.*.scp > $temp_dir/ali.scp || exit 1;

egs/wsj/s5/steps/get_ctm.sh
@@ -63,7 +63,7 @@ if [ $stage -le 0 ]; then
   fi
 
   nj=$(cat $dir/num_jobs)
-  lats=$(eval echo $dir/lat.{$(seq -s, $nj)}.gz)
+  lats=$(for n in $(seq $nj); do echo $dir/lat.$n.gz; done)
   if [ -f $lang/phones/word_boundary.int ]; then
     $cmd LMWT=$min_lmwt:$max_lmwt $dir/scoring/log/get_ctm.LMWT.log \
       set -o pipefail '&&' mkdir -p $dir/score_LMWT/ '&&' \

egs/wsj/s5/steps/nnet2/get_egs_discriminative2.sh
@@ -80,10 +80,6 @@ mkdir -p $dir/log $dir/info || exit 1;
 nj=$(cat $denlatdir/num_jobs) || exit 1; # $nj is the number of
                                          # splits of the denlats and alignments.
 
-
-[ "$(readlink /bin/sh)" == dash ] && \
-  echo "This script won't work if /bin/sh points to dash.  make it point to bash." && exit 1
-
 nj_ali=$(cat $alidir/num_jobs) || exit 1;
 
 sdata=$data/split$nj
@@ -91,16 +87,16 @@ utils/split_data.sh $data $nj
 
 if [ $nj_ali -eq $nj ]; then
   ali_rspecifier="ark,s,cs:gunzip -c $alidir/ali.JOB.gz |"
-  all_ids=$(seq -s, $nj)
-  prior_ali_rspecifier="ark,s,cs:gunzip -c $alidir/ali.{$all_ids}.gz | copy-int-vector ark:- ark,t:- | utils/filter_scp.pl $dir/priors_uttlist | ali-to-pdf $alidir/final.mdl ark,t:- ark:- |"
+  alis=$(for n in $(seq $nj); do echo $alidir/ali.$n.gz; done)
+  prior_ali_rspecifier="ark,s,cs:gunzip -c $alis | copy-int-vector ark:- ark,t:- | utils/filter_scp.pl $dir/priors_uttlist | ali-to-pdf $alidir/final.mdl ark,t:- ark:- |"
 else
   ali_rspecifier="scp:$dir/ali.scp"
   prior_ali_rspecifier="ark,s,cs:utils/filter_scp.pl $dir/priors_uttlist $dir/ali.scp | ali-to-pdf $alidir/final.mdl scp:- ark:- |"
   if [ $stage -le 1 ]; then
     echo "$0: number of jobs in den-lats versus alignments differ: dumping them as single archive and index."
-    all_ids=$(seq -s, $nj_ali)
+    alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
     $cmd $dir/log/copy_alignments.log \
-      copy-int-vector "ark:gunzip -c $alidir/ali.{$all_ids}.gz|" \
+      copy-int-vector "ark:gunzip -c $alis|" \
       ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
   fi
 fi
@@ -180,8 +176,8 @@ if [ ! -z "$transform_dir" ]; then
   else
     # number of jobs matches with alignment dir.
     feats="$feats transform-feats --utt2spk=ark:$sdata/JOB/utt2spk ark:$transform_dir/$trans.JOB ark:- ark:- |"
-    all_ids=`seq -s, $nj`
-    priors_feats="$priors_feats transform-feats --utt2spk=ark:$data/utt2spk 'ark:cat $transform_dir/$trans.{$all_ids} |' ark:- ark:- |"
+    tras=$(for n in $(seq $nj); do echo $transform_dir/$trans.$n; done)
+    priors_feats="$priors_feats transform-feats --utt2spk=ark:$data/utt2spk 'ark:cat $tras |' ark:- ark:- |"
   fi
 fi
 if [ ! -z $online_ivector_dir ]; then

egs/wsj/s5/steps/nnet2/relabel_egs.sh
@@ -53,7 +53,7 @@ echo $num_jobs_nnet > $dir/num_jobs_nnet
 echo $iters_per_epoch > $dir/iters_per_epoch
 echo $samples_per_iter_real > $dir/samples_per_iter
 
-alignments=`eval echo $alidir/ali.{$(seq -s ',' $num_jobs_align)}.gz`
+alignments=$(for n in $(seq $num_jobs_align); do echo $alidir/ali.$n.gz; done)
 
 if [ $stage -le 0 ]; then
   egs_in=

egs/wsj/s5/steps/nnet3/get_egs_discriminative.sh
@@ -102,9 +102,6 @@ done
 
 mkdir -p $dir/log $dir/info || exit 1;
 
-[ "$(readlink /bin/sh)" == dash ] && \
-  echo "This script won't work if /bin/sh points to dash.  make it point to bash." && exit 1
-
 nj=$(cat $denlatdir/num_jobs) || exit 1;
 
 sdata=$data/split$nj
@@ -132,9 +129,9 @@ awk '{print $1}' $data/utt2spk | utils/filter_scp.pl --exclude $dir/valid_uttlis
 
 if [ $stage -le 1 ]; then
   nj_ali=$(cat $alidir/num_jobs)
-  all_ids=$(seq -s, $nj_ali)
+  alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
   $cmd $dir/log/copy_alignments.log \
-    copy-int-vector "ark:gunzip -c $alidir/ali.{$all_ids}.gz|" \
+    copy-int-vector "ark:gunzip -c $alis|" \
     ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
 fi
 
@@ -324,9 +321,9 @@ fi
   if $adjust_priors && [ $stage -le 10 ]; then
     if [ ! -f $dir/ali.scp ]; then
       nj_ali=$(cat $alidir/num_jobs)
-      all_ids=$(seq -s, $nj_ali)
+      alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
       $cmd $dir/log/copy_alignments.log \
-        copy-int-vector "ark:gunzip -c $alidir/ali.{$all_ids}.gz|" \
+        copy-int-vector "ark:gunzip -c $alis|" \
         ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
     fi
 

egs/wsj/s5/steps/online/nnet2/get_egs_discriminative2.sh
@@ -85,9 +85,9 @@ else
   ali_rspecifier="scp:$dir/ali.scp"
   if [ $stage -le 1 ]; then
     echo "$0: number of jobs in den-lats versus alignments differ: dumping them as single archive and index."
-    all_ids=$(seq -s, $nj_ali)
+    alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
     copy-int-vector --print-args=false \
-      "ark:gunzip -c $alidir/ali.{$all_ids}.gz|" ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
+      "ark:gunzip -c $alis|" ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
   fi
 fi
 

egs/wsj/s5/steps/nnet2/get_egs.sh
@@ -199,7 +199,6 @@ if [ $stage -le 2 ]; then
     gzip -c >$dir/ali_special.gz || exit 1;
   set +o pipefail; # unset the pipefail option.
 
-  all_ids=$(seq -s, $nj)  # e.g. 1,2,...39,40
   $cmd $dir/log/create_valid_subset.log \
     nnet-get-egs $ivectors_opt $nnet_context_opts "$valid_feats" \
     "ark,s,cs:gunzip -c $dir/ali_special.gz | ali-to-pdf $alidir/final.mdl ark:- ark:- | ali-to-post ark:- ark:- |" \

egs/wsj/s5/steps/nnet2/relabel_egs2.sh
@@ -59,7 +59,7 @@ mkdir -p $dir/log
 mkdir -p $dir/info
 cp -r $egs_in_dir/info/*  $dir/info
 
-alignments=`eval echo $alidir/ali.{$(seq -s ',' $num_jobs_align)}.gz`
+alignments=$(for n in $(seq $num_jobs_align); do echo $alidir/ali.$n.gz; done)
 
 if [ $stage -le 0 ]; then
   for x in $(seq $num_archives); do

commit 022c312d3fc82a4365ec269be758441d2652b7bc

Fix bug regarding newlines in strings, introduced in commit d90d3545f8054efc3.

egs/tedlium/s5_r2/RESULTS
@@ -97,7 +97,7 @@ for x in exp/nnet3_cleaned/tdnn_sp/decode_*; do grep Sum $x/*ore*/*ys | utils/be
 
 
   # chain+TDNN systems ran without cleanup, using the command:
-  # local/chain/run_tdnn.sh --train-set train --gmm tri3 --nnet3-affix "" --stage 20
+  # local/chain/run_tdnn.sh --train-set train --gmm tri3 --nnet3-affix ""
   # for d in exp/chain/tdnn_sp_bi/decode_*; do grep Sum $d/*/*ys | utils/best_wer.sh; done
   # This is about 0.6% worse than the corresponding results with cleanup.
 

egs/wsj/s5/steps/combine_ali_dirs.sh
@@ -68,7 +68,7 @@ echo "$0: dumping alignments in each source directory as single archive and inde
 for dir in $*; do
   src_id=$((src_id + 1))
   cur_num_jobs=$(cat $dir/num_jobs) || exit 1;
-  alis=$(for n in $(seq $cur_num_jobs); do echo $dir/ali.$n.gz; done)
+  alis=$(for n in $(seq $cur_num_jobs); do echo -n "$dir/ali.$n.gz "; done)
   $cmd $dir/log/copy_alignments.log \
     copy-int-vector "ark:gunzip -c $alis|" \
     ark,scp:$temp_dir/ali.$src_id.ark,$temp_dir/ali.$src_id.scp || exit 1;

egs/wsj/s5/steps/get_ctm.sh
@@ -63,7 +63,7 @@ if [ $stage -le 0 ]; then
   fi
 
   nj=$(cat $dir/num_jobs)
-  lats=$(for n in $(seq $nj); do echo $dir/lat.$n.gz; done)
+  lats=$(for n in $(seq $nj); do echo -n "$dir/lat.$n.gz "; done)
   if [ -f $lang/phones/word_boundary.int ]; then
     $cmd LMWT=$min_lmwt:$max_lmwt $dir/scoring/log/get_ctm.LMWT.log \
       set -o pipefail '&&' mkdir -p $dir/score_LMWT/ '&&' \

egs/wsj/s5/steps/nnet2/get_egs_discriminative2.sh
@@ -87,14 +87,14 @@ utils/split_data.sh $data $nj
 
 if [ $nj_ali -eq $nj ]; then
   ali_rspecifier="ark,s,cs:gunzip -c $alidir/ali.JOB.gz |"
-  alis=$(for n in $(seq $nj); do echo $alidir/ali.$n.gz; done)
+  alis=$(for n in $(seq $nj); do echo -n "$alidir/ali.$n.gz "; done)
   prior_ali_rspecifier="ark,s,cs:gunzip -c $alis | copy-int-vector ark:- ark,t:- | utils/filter_scp.pl $dir/priors_uttlist | ali-to-pdf $alidir/final.mdl ark,t:- ark:- |"
 else
   ali_rspecifier="scp:$dir/ali.scp"
   prior_ali_rspecifier="ark,s,cs:utils/filter_scp.pl $dir/priors_uttlist $dir/ali.scp | ali-to-pdf $alidir/final.mdl scp:- ark:- |"
   if [ $stage -le 1 ]; then
     echo "$0: number of jobs in den-lats versus alignments differ: dumping them as single archive and index."
-    alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
+    alis=$(for n in $(seq $nj_ali); do echo -n "$alidir/ali.$n.gz "; done)
     $cmd $dir/log/copy_alignments.log \
       copy-int-vector "ark:gunzip -c $alis|" \
       ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
@@ -176,7 +176,7 @@ if [ ! -z "$transform_dir" ]; then
   else
     # number of jobs matches with alignment dir.
     feats="$feats transform-feats --utt2spk=ark:$sdata/JOB/utt2spk ark:$transform_dir/$trans.JOB ark:- ark:- |"
-    tras=$(for n in $(seq $nj); do echo $transform_dir/$trans.$n; done)
+    tras=$(for n in $(seq $nj); do echo -n "$transform_dir/$trans.$n "; done)
     priors_feats="$priors_feats transform-feats --utt2spk=ark:$data/utt2spk 'ark:cat $tras |' ark:- ark:- |"
   fi
 fi
@@ -278,7 +278,7 @@ fi
 if [ $stage -le 3 ]; then
   echo "$0: getting initial training examples by splitting lattices"
 
-  degs_list=$(for n in $(seq $num_archives_temp); do echo ark:$dir/degs_orig.JOB.$n.ark; done)
+  degs_list=$(for n in $(seq $num_archives_temp); do echo -n "ark:$dir/degs_orig.JOB.$n.ark "; done)
 
   $cmd JOB=1:$nj $dir/log/get_egs.JOB.log \
     nnet-get-egs-discriminative --criterion=$criterion --drop-frames=$drop_frames \
@@ -289,7 +289,7 @@ fi
 
 if [ $stage -le 4 ]; then
 
-  degs_list=$(for n in $(seq $nj); do echo $dir/degs_orig.$n.JOB.ark; done)
+  degs_list=$(for n in $(seq $nj); do echo -n "$dir/degs_orig.$n.JOB.ark "; done)
 
   if [ $num_archives -eq $num_archives_temp ]; then
     echo "$0: combining data into final archives and shuffling it"
@@ -308,7 +308,7 @@ if [ $stage -le 4 ]; then
     # ... num_archives, which is more than num_archives_temp.  The list with
     # \$[... ] expressions in it computes the set of final indexes for each
     # temporary index.
-    degs_list_out=$(for n in $(seq $archive_ratio); do echo "ark:$dir/degs_temp.\$[((JOB-1)*$archive_ratio)+$n].ark"; done)
+    degs_list_out=$(for n in $(seq $archive_ratio); do echo -n "ark:$dir/degs_temp.\$[((JOB-1)*$archive_ratio)+$n].ark "; done)
     # e.g. if dir=foo and archive_ratio=2, we'd have
     # degs_list_out='foo/degs_temp.$[((JOB-1)*2)+1].ark foo/degs_temp.$[((JOB-1)*2)+2].ark'
 

egs/wsj/s5/steps/nnet2/relabel_egs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Copyright 2014  Vimal Manohar. Apache 2.0.
-# This script, which will generally be called during the neural-net training 
+# This script, which will generally be called during the neural-net training
 # relabels existing examples with better labels obtained by realigning the data
 # with the current nnet model
 
 # Begin configuration section
 cmd=run.pl
 stage=0
-extra_egs=        # Names of additional egs files that need to relabelled 
+extra_egs=        # Names of additional egs files that need to relabelled
                   # other than egs.*.*.ark, combine.egs, train_diagnostic.egs,
                   # valid_diagnostic.egs
 iter=final
@@ -53,7 +53,7 @@ echo $num_jobs_nnet > $dir/num_jobs_nnet
 echo $iters_per_epoch > $dir/iters_per_epoch
 echo $samples_per_iter_real > $dir/samples_per_iter
 
-alignments=$(for n in $(seq $num_jobs_align); do echo $alidir/ali.$n.gz; done)
+alignments=$(for n in $(seq $num_jobs_align); do echo -n "$alidir/ali.$n.gz "; done)
 
 if [ $stage -le 0 ]; then
   egs_in=

egs/wsj/s5/steps/nnet3/get_egs_discriminative.sh
@@ -129,7 +129,7 @@ awk '{print $1}' $data/utt2spk | utils/filter_scp.pl --exclude $dir/valid_uttlis
 
 if [ $stage -le 1 ]; then
   nj_ali=$(cat $alidir/num_jobs)
-  alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
+  alis=$(for n in $(seq $nj_ali); do echo -n "$alidir/ali.$n.gz "; done)
   $cmd $dir/log/copy_alignments.log \
     copy-int-vector "ark:gunzip -c $alis|" \
     ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
@@ -321,7 +321,7 @@ fi
   if $adjust_priors && [ $stage -le 10 ]; then
     if [ ! -f $dir/ali.scp ]; then
       nj_ali=$(cat $alidir/num_jobs)
-      alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
+      alis=$(for n in $(seq $nj_ali); do echo -n "$alidir/ali.$n.gz "; done)
       $cmd $dir/log/copy_alignments.log \
         copy-int-vector "ark:gunzip -c $alis|" \
         ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
@@ -426,7 +426,7 @@ if [ $stage -le 6 ]; then
     # otherwise managing the output names is quite difficult (and we don't want
     # to submit separate queue jobs for each intermediate archive, because then
     # the --max-jobs-run option is hard to enforce).
-    output_archives="$(for y in $(seq $archives_multiple); do echo ark:$dir/degs.JOB.$y.ark; done)"
+    output_archives=$(for y in $(seq $archives_multiple); do echo -n "ark:$dir/degs.JOB.$y.ark "; done)
     for x in $(seq $num_archives_intermediate); do
       for y in $(seq $archives_multiple); do
         archive_index=$[($x-1)*$archives_multiple+$y]

egs/wsj/s5/steps/online/nnet2/get_egs_discriminative2.sh
@@ -85,7 +85,7 @@ else
   ali_rspecifier="scp:$dir/ali.scp"
   if [ $stage -le 1 ]; then
     echo "$0: number of jobs in den-lats versus alignments differ: dumping them as single archive and index."
-    alis=$(for n in $(seq $nj_ali); do echo $alidir/ali.$n.gz; done)
+    alis=$(for n in $(seq $nj_ali); do echo -n "$alidir/ali.$n.gz "; done)
     copy-int-vector --print-args=false \
       "ark:gunzip -c $alis|" ark,scp:$dir/ali.ark,$dir/ali.scp || exit 1;
   fi
@@ -121,7 +121,7 @@ if [ $stage -le 2 ]; then
   echo "$0: working out number of frames of training data"
   num_frames=$(steps/nnet2/get_num_frames.sh $data)
 
-  echo $num_frames > $dir/info/num_frames 
+  echo $num_frames > $dir/info/num_frames
 
   # Working out total number of archives. Add one on the assumption the
   # num-frames won't divide exactly, and we want to round up.
@@ -138,7 +138,7 @@ if [ $stage -le 2 ]; then
 
   echo $num_archives >$dir/info/num_archives || exit 1
   echo $num_archives_temp >$dir/info/num_archives_temp || exit 1
-  
+
   frames_per_archive=$[$num_frames/$num_archives]
 
   # note, this is the number of frames per archive prior to discarding frames.
@@ -176,7 +176,7 @@ fi
 if [ $stage -le 3 ]; then
   echo "$0: getting initial training examples by splitting lattices"
 
-  degs_list=$(for n in $(seq $num_archives_temp); do echo ark:$dir/degs_orig.JOB.$n.ark; done)
+  degs_list=$(for n in $(seq $num_archives_temp); do echo -n "ark:$dir/degs_orig.JOB.$n.ark "; done)
 
   $cmd JOB=1:$nj $dir/log/get_egs.JOB.log \
     nnet-get-egs-discriminative --criterion=$criterion --drop-frames=$drop_frames \
@@ -186,12 +186,12 @@ if [ $stage -le 3 ]; then
 fi
 
 if [ $stage -le 4 ]; then
-  
-  degs_list=$(for n in $(seq $nj); do echo $dir/degs_orig.$n.JOB.ark; done)
+
+  degs_list=$(for n in $(seq $nj); do echo -n "$dir/degs_orig.$n.JOB.ark "; done)
 
   if [ $num_archives -eq $num_archives_temp ]; then
     echo "$0: combining data into final archives and shuffling it"
-    
+
     $cmd JOB=1:$num_archives $dir/log/shuffle.JOB.log \
       cat $degs_list \| nnet-shuffle-egs-discriminative --srand=JOB ark:- \
        ark:$dir/degs.JOB.ark || exit 1;
@@ -206,7 +206,7 @@ if [ $stage -le 4 ]; then
     # ... num_archives, which is more than num_archives_temp.  The list with
     # \$[... ] expressions in it computes the set of final indexes for each
     # temporary index.
-    degs_list_out=$(for n in $(seq $archive_ratio); do echo "ark:$dir/degs_temp.\$[((JOB-1)*$archive_ratio)+$n].ark"; done)
+    degs_list_out=$(for n in $(seq $archive_ratio); do echo -n "ark:$dir/degs_temp.\$[((JOB-1)*$archive_ratio)+$n].ark "; done)
     # e.g. if dir=foo and archive_ratio=2, we'd have
     # degs_list_out='foo/degs_temp.$[((JOB-1)*2)+1].ark foo/degs_temp.$[((JOB-1)*2)+2].ark'
 

egs/wsj/s5/utils/split_scp.pl
@@ -206,7 +206,7 @@ if ($utt2spk_file ne "") {  # We have the --utt2spk option...
         $error = 1;
     }
     $linesperscp = int( $numlines / $numscps); # the "whole part"..
-    $linesperscp >= 1 || die "You are splitting into too many pieces!";
+    $linesperscp >= 1 || die "You are splitting into too many pieces! [reduce \$nj]";
     $remainder = $numlines - ($linesperscp * $numscps);
     ($remainder >= 0 && $remainder < $numlines) || die "bad remainder $remainder";
     # [just doing int() rounds down].