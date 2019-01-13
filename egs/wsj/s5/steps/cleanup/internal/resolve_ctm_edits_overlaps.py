akshaysubr/kaldi

commit 543d39b0b89c378693dc5753f5c6cce0057b4b78

[scripts,egs] Changes for Python 2/3 compatibility (#2925)

egs/wsj/s5/steps/cleanup/internal/resolve_ctm_edits_overlaps.py
@@ -15,6 +15,7 @@ in the two overlapping segments, and chooses the better one.
 """
 
 from __future__ import print_function
+from __future__ import division
 import argparse
 import collections
 import logging
@@ -228,7 +229,7 @@ def resolve_overlaps(ctm_edits, segments):
             try:
                 cur_utt_end_index = next(
                     (i for i, line in enumerate(ctm_edits_for_cur_utt)
-                     if line[2] + line[3] / 2.0 > window_length - overlap))
+                     if line[2] + line[3] / 2.0)> window_length - overlap))
             except StopIteration:
                 cur_utt_end_index = len(ctm_edits_for_cur_utt)
 
@@ -299,7 +300,7 @@ def run(args):
     segments, reco2utt = read_segments(args.segments)
     ctm_edits = read_ctm_edits(args.ctm_edits_in, segments)
 
-    for reco, utts in reco2utt.iteritems():
+    for reco, utts in reco2utt.items():
         ctm_edits_for_reco = []
         for utt in sorted(utts, key=lambda x: segments[x][1]):
             if (reco, utt) in ctm_edits:

commit b300c3bdf890d2dcbbf016f5302dc7c93869f459

[scripts] Fix typo introduced in #2925 (#2989)

egs/wsj/s5/steps/cleanup/internal/resolve_ctm_edits_overlaps.py
@@ -229,7 +229,7 @@ def resolve_overlaps(ctm_edits, segments):
             try:
                 cur_utt_end_index = next(
                     (i for i, line in enumerate(ctm_edits_for_cur_utt)
-                     if line[2] + line[3] / 2.0)> window_length - overlap))
+                     if line[2] + line[3] / 2.0 > window_length - overlap))
             except StopIteration:
                 cur_utt_end_index = len(ctm_edits_for_cur_utt)
 