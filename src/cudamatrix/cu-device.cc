25048b3b28d1695d6166f9de6a2e02dcc3002ec5
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2020-10-07 21:39:22 -0700

    Enabling TF32 compute mode by default when using CUDA 11 or greater

--- src/cudamatrix/cu-device.cc
+++ src/cudamatrix/cu-device.cc
@@ -118,12 +118,19 @@ void CuDevice::Initialize() {
 #endif
     
 #if CUDA_VERSION >= 9000 
+#if CUDA_VERSION >= 11000
+    // Enable TF32 compute mode in CUBLAS by default for tensor core acceleration
+    // Note if the device does not support tensor cores this will fall back to normal math mode
+    CUBLAS_SAFE_CALL(cublasSetMathMode(cublas_handle_, 
+          CUBLAS_TF32_TENSOR_OP_MATH));
+#else
     if (device_options_.use_tensor_cores) {
       // Enable tensor cores in CUBLAS
       // Note if the device does not support tensor cores this will fall back to normal math mode
       CUBLAS_SAFE_CALL(cublasSetMathMode(cublas_handle_, 
             CUBLAS_TENSOR_OP_MATH));
     }
+#endif
 #endif
 
     // Initialize the cuSPARSE library
@@ -278,12 +285,19 @@ void CuDevice::FinalizeActiveGpu() {
 #endif
 
 #if CUDA_VERSION >= 9000 
+#if CUDA_VERSION >= 11000
+    // Enable TF32 compute mode in CUBLAS by default for tensor core acceleration
+    // Note if the device does not support tensor cores this will fall back to normal math mode
+    CUBLAS_SAFE_CALL(cublasSetMathMode(cublas_handle_, 
+          CUBLAS_TF32_TENSOR_OP_MATH));
+#else
     if (device_options_.use_tensor_cores) {
       // Enable tensor cores in CUBLAS
       // Note if the device does not support tensor cores this will fall back to normal math mode
       CUBLAS_SAFE_CALL(cublasSetMathMode(cublas_handle_, 
             CUBLAS_TENSOR_OP_MATH));
     }
+#endif
 #endif
 
     
